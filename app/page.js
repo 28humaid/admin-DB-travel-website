710bfc7cfbf69a3668c82eef0b58598986adf7ba
Humaid Alam Ansari <28.humaedaalam@gmail.com>
2025-10-06 22:19:04 +0530

    build fail hogyi thi...ab shayd chal jaye

--- app/page.js
+++ app/page.js
@@ -1,25 +1,6 @@
-"use client";
-
-import { useSession } from "next-auth/react";
-import { usePathname, useRouter } from "next/navigation";
-import { useEffect } from "react";
 import LoginComponent from "@/components/login/loginComponent";
 
 export default function page() {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-  const pathname = usePathname();
-
-  useEffect(() => {
-    // Only redirect if session exists AND user is really on "/"
-    if (status === "authenticated" && pathname === "/") {
-      router.replace("/admin/dashboard/createUser");
-    }
-  }, [status, router, pathname]);
-
-  if (status === "loading") {
-    return <div>Waiting...</div>;
-  }
   return (
     <LoginComponent user="admin"/>
   );
--- components/login/loginComponent.jsx
+++ components/login/loginComponent.jsx
@@ -1,6 +1,25 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
 import AdminLoginForm from "./adminLoginForm"
 
 const LoginComponent = ({user}) => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    // Only redirect if session exists AND user is really on "/"
+    if (status === "authenticated" && pathname === "/") {
+      router.replace("/admin/dashboard/createUser");
+    }
+  }, [status, router, pathname]);
+
+  if (status === "loading") {
+    return <div>Waiting...</div>;
+  }
   return (
     <div className="flex items-center justify-center min-h-screen">
         <div className="bg-blue-300 max-w-[460px] w-full mx-4 rounded-lg shadow-xl">
